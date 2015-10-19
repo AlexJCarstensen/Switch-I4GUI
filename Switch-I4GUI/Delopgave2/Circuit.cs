a6bd28a0be4ebd9f375e18b83fc04b37d732d878
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-19 16:01:37 +0200

    delopgave 2 init added functionality to Connection

---
+++ Switch-I4GUI/Delopgave2/Circuit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Delopgave2
+{
+    class Circuit
+    {
+        private enum CurrentWay
+        {
+            No,
+            Way1,
+            Way2
+        };
+        
+        Image _imLamp = new Image();
+
+        private BitmapImage _imLtOn;
+        private BitmapImage _imLtOff;
+
+        private Switch _switch1;
+        private Switch _switch2;
+
+        private Connection outlet2Lamp;
+        private Connection lamp2Sw1con1;
+        private Connection swi1Con22sw2con3;
+        private Connection sw1Con32Sw2Con2;
+        private Connection sw2Con12Outlet;
+
+        private bool _lightOn;
+
+        public Circuit(Canvas canvas)
+        {
+            
+        }
+    }
+    
+}
---
+++ Switch-I4GUI/Delopgave2/Connection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Delopgave2
+{
+    class Connection
+    {
+
+        private bool _current = false;
+        private readonly Polyline _pl;
+
+        public Connection(Canvas canvas, params Point[] pts)
+        {
+            _pl = new Polyline
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+
+            PointCollection myPointCollection = new PointCollection();
+            foreach (var p in pts)
+                myPointCollection.Add(p);
+
+            _pl.Points = myPointCollection;
+            canvas.Children.Add(_pl);
+        }
+
+        public bool Current
+        {
+            get { return _current;  }
+            set
+            {
+                _current = value;
+                _pl.Stroke = _current ? Brushes.Red : Brushes.Black;
+            }
+        }
+    }
+}
